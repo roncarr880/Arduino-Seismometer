20a02c463b7120b12f91a262da2b9afdaeb5f081
roncarr880 <roncarr880@gmail.com>
2018-02-09 15:37:21 -0500

    Add files via upload

--- Seismometer.ino
+++ Seismometer.ino
@@ -6,6 +6,11 @@
  *   
  *   LM324 op-amps cannot drive to the positive rail.  The 3.3 voltage source is used as the AREF to keep
  *   the A/D converter in the active region of the op-amp when powered by 5 volts.
+ *   
+ *   Feb 2018 - a simple FIR filter added to filter 60 hz energy in the signal.  Sample rate increased to 240 hz.
+ *   At counts 0 - 6, samples are saved.  The samples are filtered and one value is output to the serial port.
+ *   At counts 7 - 39, the samples are ignored.  Decimation is 40 but a decimation filter is not used.
+ *   The rate of samples sent to the PC is still 6 per second.
  */
 
 
@@ -13,133 +18,161 @@
 
 const int led = LED_BUILTIN;  // the pin with a LED
 volatile byte sample_ready;
-volatile int sig0;
-volatile int sig1;
-volatile int sig2;
-volatile byte sat0, sat1, sat2;
-int zp0, zp1, zp2;            // slow tracking of zero points for each A/D
-unsigned int quake, quake_detect;
+volatile byte tick;
+int sig0;
+int sig1;
+int sig2;
 
-void setup(void)
-{
+int zp0, zp1, zp2;            // slow tracking of zero points for each A/D
+int samples[7];
+
+byte sec_, min_,hour_;
+byte quakes;
+
+void setup(void){
   pinMode(led, OUTPUT);
-  Timer1.initialize(166667);
+  Timer1.initialize(166667/40);
   Timer1.attachInterrupt(sample);
   Serial.begin(9600);
   analogReference(EXTERNAL);        // 3.3 volts
   zp0 = zp1 = zp2 = 512;
 }
 
-void sample(void){    // interrupt 6 times a second
+void sample(void){    // interrupt 240 times a second.
+int val,f,zp;
+static byte counter;
 
-  sig0 = analogRead(A0);    // the signal with the most amplitude
-  sig1 = analogRead(A1);    // 2 bits down in amplitude
-  sig2 = analogRead(A2);    // another 2 bits down in amplitude
+  if( ++counter > 39 ) counter = 0;    // aquire 6 samples at 240 hz rate, skip processing discarded samples
+  if( counter < 7 ){ 
+    sig0 = analogRead(A0);    // the signal with the most amplitude
+    sig1 = analogRead(A1);    // 2 bits down in amplitude
+    sig2 = analogRead(A2);    // another 2 bits down in amplitude
   // sig1 is inverted in phase from the other 2, so correct the reading
-  sig1 = 1023 - sig1;
+    sig1 = 1023 - sig1;
+    
+  // pick a signal that is not saturating the A/D converter
+    f = 0, val = sig0, zp = zp0;
+    if( sig0 < 30 || sig0 > 1000 ) f = 2, val = sig1, zp = zp1;
+    if( sig1 < 30 || sig1 > 1000 ) f = 4, val = sig2, zp = zp2;
+  //  correct value to +- 0 point and scale
+    val = val - zp;
+    val <<= f;
 
-  sat0 = sat1 = sat2 = 0;   // the op-amp may not drive completely to zero or 1023
-  if( sig0 < 30 || sig0 > 1000 ) sat0 = 1;
-  if( sig1 < 30 || sig1 > 1000 ) sat1 = 1;
-  if( sig2 < 30 || sig2 > 1000 ) sat2 = 1;
-  
-  sample_ready = 1;
-
+    samples[counter] = val;
+    if( counter == 6 && sample_ready == 1 ) digitalWrite(led,HIGH);   // out of gas !!!
+    if( counter == 6 ) sample_ready = 1;
+  }
+  else if( tick ){             // adjust the zero points once per minute during decimation count 7 to 39
+    tick = 0;                  // when processor is lightly loaded
+    if( sig0 > zp0 ) ++zp0;
+    if( sig0 < zp0 ) --zp0; 
+    if( sig1 > zp1 ) ++zp1;
+    if( sig1 < zp1 ) --zp1;
+    if( sig2 > zp2 ) ++zp2;
+    if( sig2 < zp2 ) --zp2;
+  }
 }
 
 
 void loop(void){
+static int val;
+static byte counter;
+static byte flash_count;
 
-int v0, v1, v2;
-int val;
-int f;        // shift or mult factor
+   if( sample_ready ){                             // 6 times a second
+      val = filter();
+      noInterrupts();
+      sample_ready = 0;
+      interrupts();
+      Serial.println(val);
+      ++counter;
+      flash_count = flash_led(flash_count);
+      if( flash_count == 0 ) signal_level(val);     // signal level also turns on the led
+   }
 
-static unsigned long timer;
-static unsigned int sec_,min_,hour_;
-static byte tick;
-static byte led_holdoff;
+   if( counter == 6 ){   // one second time
+      counter = 0;
+      keep_time();
+      flash_count = quakes << 1;                    // twice amount to count both on and off times
+      quake_detect(val);
+   }
+   
+}
 
-  noInterrupts();
-  if( sample_ready ){
-    sample_ready = 0;
-    
-    if( tick ){           // top of the minute, look at zero points
-      tick = 0;
-      if( sat0 == 0 ){
-        if( sig0 > zp0 ) ++zp0;    // miving these once per minute is very slow
-        if( sig0 < zp0 ) --zp0;    // it will take 500 minutes to move half the A/D range
-      }
-      if( sat1 == 0 ){
-        if( sig1 > zp1 ) ++zp1;
-        if( sig1 < zp1 ) --zp1;
-      }
-      if( sat2 == 0 ){
-        if( sig2 > zp2 ) ++zp2;
-        if( sig2 < zp2 ) --zp2;
-      }
-    }
-    
-    v0 = sig0 - zp0;   v1 = sig1 - zp1;  v2 = sig2 - zp2;
+byte flash_led( byte count ){ // flash the number of quakes detected
+static byte led_state;
+
+  if( count == 0 ) return 0;
+  
+  led_state ^= 1;
+  digitalWrite(led,led_state);
+  return (count - 1); 
+}
+
+void keep_time(){            // called once a second
+
+  if( ++sec_ == 60 ){
+    sec_ = 0;
+    noInterrupts();
+    tick = 1;                // flag to run the zero point code
     interrupts();
-
-    // which signal is not saturated
-    val = v2;   f = 4;              // default.  We will use this one even if it is saturated
-    if( sat1 == 0 ) val = v1, f = 2;
-    if( sat0 == 0 ) val = v0, f = 0;  
-
-    val = val << f;                 // shift up the signals sampled at lower amplification
-
-    //Serial.print(zp0);  Serial.write(' ');
-    Serial.println(val);            // serial data for AmaSeis program
-
-    //Serial.print(sig0); Serial.write(' ');
-    //Serial.print(sig1); Serial.write(' ');
-    //Serial.println(sig2);
-
-    // light the LED if it looks like we are receiving an earthquake
-    if( val > 20 || val < -20 ){
-      digitalWrite(led,HIGH);
-      if( hour_ > 2 ) ++quake_detect;     // avoid false detect on startup
-    }
-    else{
-      if( led_holdoff == 0 ) digitalWrite(led,LOW);
-      if( quake_detect ) --quake_detect;
-    }
-    
-    if( quake_detect > 3600 ){             // large signals for 10 minutes
-      ++quake;
-      quake_detect = 0;
-      hour_ = 2;             // no more detects for an hour
-    }
-  }
-  
-  interrupts();
-
-  // keeping track of some time
-  if( timer != millis() ){
-    timer = millis();
-    
-    if( led_holdoff && (timer % 250) == 0 ) led_holdoff = 0;  // allow led out 1/4 second after on
-    
-    if( ( timer % 1000 ) == 0 ){
-      // each second blink the led if we have detected quakes in the past
-      if( quake ) digitalWrite(led,HIGH), led_holdoff = 1; 
-      
-      if( ++sec_ == 60 ){
-        sec_ = 0,
-        tick = 1;
-        if( ++min_ == 60 ){
-          min_ = 0;
-          ++hour_;
-          if( hour_ > 26 ){        // time out quakes, display for one day
-            hour_ = 3;
-            if( quake ) --quake;
-          }
-        }
+    if( ++min_ == 60 ){
+      min_ = 0;
+      if(++hour_ == 24 ){
+        if( quakes ) --quakes;   // removed quake detected after 24 hours of quiet
+        hour_ = 0;
       }
     }
   }
   
+}
+
+void signal_level(int val){     // led on if value is more than some amount
+static byte led_on;             // did this routine turn the led on
+                                // 3 processes are writing the led, so it is a bit overloaded.
+
+  if( val > 50 || val < -50 ) led_on = 1, digitalWrite(led,HIGH);
+  else if( led_on ) led_on = 0, digitalWrite(led,LOW);
+}
+
+void quake_detect( int val ){       // called once a second
+static int detector;
+static int holdoff;
+
+  if( holdoff ){              // avoid counting the same quake twice
+    --holdoff;
+    return;
+  }
+
+  if( val > 20 || val < -20 ) ++detector;
+  else if( detector > 0 ) --detector;
   
+  if( detector > 300 ){
+    if( quakes < 3 ) ++quakes;       // can only blink 3 a second, so not counting above 3
+    detector = 0;
+    hour_ = 0;                       // timeout in 24 hours
+    holdoff = 3600;                  // no detects for next hour
+  }
+  
+}
+
+
+// run a simple FIR filter to reject 60 hz energy.  Coefficients are .25 .5 .75 1 .75 .5 .25
+int filter(){
+int val;        // if all these samples are max, then 4 * 8192 == 32k, at the limit of an integer.
+                // in theory there should be no overflow
+
+   val = samples[0] >> 2;
+   val += samples[1] >> 1;
+   val += samples[2] >> 2;
+   val += samples[2] >> 1;
+   val += samples[3];
+   val += samples[4] >> 1;
+   val += samples[4] >> 2;
+   val += samples[5] >> 1;
+   val += samples[6] >> 2;
+   val >>= 1;                // filter has a gain of 4, returning a times by 2 value
+
+   return val;
 }
 
