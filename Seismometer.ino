fc2c00c00d08497d09099f001221cd6bb5b5e645
roncarr880 <roncarr880@gmail.com>
2018-02-11 17:09:39 -0500

    Add files via upload

--- Seismometer.ino
+++ Seismometer.ino
@@ -11,6 +11,10 @@
  *   At counts 0 - 6, samples are saved.  The samples are filtered and one value is output to the serial port.
  *   At counts 7 - 39, the samples are ignored.  Decimation is 40 but a decimation filter is not used.
  *   The rate of samples sent to the PC is still 6 per second.
+ *   
+ *   New zero point algorithm to avoid false 60 second signals.
+ *   The LED will flash when the signal is over a certain level. When quakes have been detected, the number of 
+ *   quakes is flashed in the LED once a second.
  */
 
 
@@ -24,25 +28,34 @@ int sig1;
 int sig2;
 
 int zp0, zp1, zp2;            // slow tracking of zero points for each A/D
-int samples[7];
+int samples[7];               // FIR filter samples
+int zero_points[64];          // save an hours worth of values
+int zpin;
 
 byte sec_, min_,hour_;
 byte quakes;
+int holdoff;                  // the zero point and quake detect code is skipped at certain times.
+int zp_holdoff;
 
 void setup(void){
   pinMode(led, OUTPUT);
-  Timer1.initialize(166667/40);
+  Timer1.initialize(166667/40);     // 240 hz
   Timer1.attachInterrupt(sample);
   Serial.begin(9600);
   analogReference(EXTERNAL);        // 3.3 volts
   zp0 = zp1 = zp2 = 512;
-}
+  for( zpin = 0; zpin < 64; ++zpin) zero_points[zpin] = 512;
+  zpin = 0;
+  zp_holdoff = 10;                   // don't run zero point algorithms on startup. Delay 10 minutes.
+  holdoff = 3600 * 2;                // don't run quake detect, delay 2 hour.
+} 
 
 void sample(void){    // interrupt 240 times a second.
-int val,f,zp;
+int val,zp;
 static byte counter;
+static byte f;
 
-  if( ++counter > 39 ) counter = 0;    // aquire 6 samples at 240 hz rate, skip processing discarded samples
+  if( ++counter > 39 ) counter = 0;    // aquire 7 samples at 240 hz rate, skip processing discarded samples
   if( counter < 7 ){ 
     sig0 = analogRead(A0);    // the signal with the most amplitude
     sig1 = analogRead(A1);    // 2 bits down in amplitude
@@ -64,9 +77,11 @@ static byte counter;
   }
   else if( tick ){             // adjust the zero points once per minute during decimation count 7 to 39
     tick = 0;                  // when processor is lightly loaded
-    if( sig0 > zp0 ) ++zp0;
-    if( sig0 < zp0 ) --zp0; 
-    if( sig1 > zp1 ) ++zp1;
+    if( f == 0 ){              // move this zero point only when not in saturation
+       zero_points[zpin++] = sig0;  // save some numbers to average for the most used signal
+       zpin &= 63;
+    }
+    if( sig1 > zp1 ) ++zp1;    // simple algorithm for the other 2
     if( sig1 < zp1 ) --zp1;
     if( sig2 > zp2 ) ++zp2;
     if( sig2 < zp2 ) --zp2;
@@ -86,8 +101,8 @@ static byte flash_count;
       interrupts();
       Serial.println(val);
       ++counter;
-      flash_count = flash_led(flash_count);
-      if( flash_count == 0 ) signal_level(val);     // signal level also turns on the led
+      flash_count = flash_led(flash_count);         // turn LED on and off at 6 hz rate if have quakes detected
+      if( flash_count == 0 ) signal_level(val);     // signal level also turns on the led if no quakes
    }
 
    if( counter == 6 ){   // one second time
@@ -99,7 +114,7 @@ static byte flash_count;
    
 }
 
-byte flash_led( byte count ){ // flash the number of quakes detected
+byte flash_led( byte count ){ // flash the LED up to 3 times a second. Called 6 times a second.
 static byte led_state;
 
   if( count == 0 ) return 0;
@@ -113,8 +128,10 @@ void keep_time(){            // called once a second
 
   if( ++sec_ == 60 ){
     sec_ = 0;
+    calc_zp0();
     noInterrupts();
-    tick = 1;                // flag to run the zero point code
+    if( zp_holdoff ) --zp_holdoff;
+    else tick = 1;                          // flag to run the zero point code
     interrupts();
     if( ++min_ == 60 ){
       min_ = 0;
@@ -127,6 +144,18 @@ void keep_time(){            // called once a second
   
 }
 
+
+void calc_zp0(){               // average of an hour of data
+long sum;                      // weakness of this algorithm is it takes an hour to replace
+byte i;                        // one bad point of data.
+   sum = 0;
+   for( i = 0; i < 64; ++i ) sum += zero_points[i];
+   sum >>= 6;
+   noInterrupts();
+   zp0 = sum;
+   interrupts();
+}
+
 void signal_level(int val){     // led on if value is more than some amount
 static byte led_on;             // did this routine turn the led on
                                 // 3 processes are writing the led, so it is a bit overloaded.
@@ -137,7 +166,7 @@ static byte led_on;             // did this routine turn the led on
 
 void quake_detect( int val ){       // called once a second
 static int detector;
-static int holdoff;
+
 
   if( holdoff ){              // avoid counting the same quake twice
     --holdoff;
@@ -150,8 +179,9 @@ static int holdoff;
   if( detector > 300 ){
     if( quakes < 3 ) ++quakes;       // can only blink 3 a second, so not counting above 3
     detector = 0;
-    hour_ = 0;                       // timeout in 24 hours
-    holdoff = 3600;                  // no detects for next hour
+    hour_ = 0;                         // timeout in 24 hours in keep_time()
+    holdoff = 3600;                   // no detects for next hour
+    zp_holdoff = 30;                  // zero points hold off also while receiving a quake
   }
   
 }
